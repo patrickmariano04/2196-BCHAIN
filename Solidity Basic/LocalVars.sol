985752a9b1c51040f138528fe05507ee13a75538
Pat Mariano <99706405+patrickmariano04@users.noreply.github.com>
2024-02-07 22:48:15 +0800

    Add files via upload

---
+++ Solidity Basic/LocalVars.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+contract LocalVariables{
+    uint public i;
+    bool public b;
+    address public myAddress;
+
+    function foo()external {
+        uint x = 123;
+        bool f = false;
+        x+= 456;
+        f= true;
+        i= 123;
+        b= false;
+        myAddress = address(1);
+    }
+}
